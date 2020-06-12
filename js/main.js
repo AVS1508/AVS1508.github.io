fbdf68e0ac89bb066b8fe62f3ad6c221a08f1468
AVS1508 <aditya.11.present@gmail.com>
2020-06-12 18:15:58 +0530

    Resolved spacing issues

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Display of loading screen and adjustment of body elements with respect to navigation bar, while loading the page in browser.
 $(window).on("load", function () {
+  $("body").css("padding-top", $("#navmenu").height());
   $("#outerNavbar ul li a").click(function (e) {
     e.preventDefault();
     let navigationHeight = $("#navmenu").outerHeight();
