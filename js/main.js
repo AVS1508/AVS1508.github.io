3938111eea25d9f2d1cb7adc4b33a671b4453f2d
AVS1508 <aditya.11.present@gmail.com>
2020-06-12 17:42:51 +0530

    Navbar fixes

--- js/main.js
+++ js/main.js
@@ -1,39 +1,48 @@
 // Display of loading screen and adjustment of body elements with respect to navigation bar, while loading the page in browser.
-$(window).load(function () {
-    $(".se-pre-con").fadeOut(300);
-    $('body').css('padding-top', $('#navbar').height() - 53);
+$(window).on("load", function () {
+  $("#outerNavbar ul li a").click(function (e) {
+    e.preventDefault();
+    let navigationHeight = $("#navmenu").outerHeight();
+    let myHref = $(this).attr("href");
+    let newPos = $(myHref).offset().top;
+    window.scroll(0, newPos - navigationHeight);
+  });
 });
 
 // Selection of active navlink in navigation bar, while scrolling through the webpage.
-$(window).scroll(function () {
-    var scrollDistance = $(window).scrollTop();
-    $('.navsection').each(function (i) {
-        if ($(this).position().top <= scrollDistance) {
-            $('#navbar li a.active').removeClass('active');
-            $('#navbar li a').eq(i).addClass('active');
-        }
+$(window)
+  .scroll(function () {
+    let navigationHeight = $("#navmenu").outerHeight();
+    let scrollDistance = $(window).scrollTop();
+    $(".navsection").each(function (i) {
+      console.log($(this).position().top);
+      if ($(this).position().top <= scrollDistance + navigationHeight) {
+        $(".navbar-nav").find("li.active").removeClass("active");
+        $(".navbar-nav li").eq(i).addClass("active");
+      }
     });
-}).scroll();
+  })
+  .scroll();
 
 // Adjustment of body elements with respect to navigation bar, while resizing browser window.
 $(window).resize(function () {
-    $('body').css('padding-top', $('#navbar').height() - 53);
+  $("body").css("padding-top", $("#navbar").height() - 53);
 });
 
 // Opening (and activating) of the relevant Skillset tab and closing all other Skillset tabs.
 function openSkill(evt, skillName) {
-    var i, tabcontent, tablinks;
+  let i, tabcontent, tablinks;
 
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
 
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
 
-    document.getElementById(skillName).style.display = "block";
-    evt.currentTarget.className += " active";
+  document.getElementById(skillName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
