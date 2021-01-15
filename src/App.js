b25b86354bec4f4df9911d28cde2d8a0013ae4f2
AVS1508 <aditya.11.present@gmail.com>
2021-01-15 13:15:59 +0530

    Switched from BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavigationBar from "./components/NavigationBar";
 import Footer from "./components/Footer";
