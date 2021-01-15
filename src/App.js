16574248357e61ff3698803722a1820677807d43
AVS1508 <aditya.11.present@gmail.com>
2021-01-15 13:37:24 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavigationBar from "./components/NavigationBar";
 import Footer from "./components/Footer";
