e7f484b9d8adc1584abb12358bbdcdbcb560f101
AVS1508 <aditya.11.present@gmail.com>
2021-07-29 09:52:20 +0530

    Formatting resolution for App.js

--- src/App.js
+++ src/App.js
@@ -14,56 +14,56 @@ const Achievements = lazy(() => import("./pages/Achievements"));
 const Error404 = lazy(() => import("./pages/Error404"));
 
 const Loading = () => {
-  return (
-    <div className="center outer-structure" style={{ display: "flex", flexDirection: "column" }}>
-      <Spinner animation="border" variant="dark" />
-    </div>
-  );
+    return (
+        <div className="center outer-structure" style={{ display: "flex", flexDirection: "column" }}>
+            <Spinner animation="border" variant="dark" />
+        </div>
+    );
 };
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentTab: "home",
-    };
-    this.componentList = ["home", "about", "education", "experience", "projects", "skills", "achievements"];
-  }
-
-  setTab = (newTab) => {
-    this.setState({ currentTab: newTab });
-  };
-
-  renderSwitch(currentTab) {
-    switch (currentTab) {
-      case "home":
-        return <Home />;
-      case "about":
-        return <About />;
-      case "education":
-        return <Education />;
-      case "experience":
-        return <Experience />;
-      case "projects":
-        return <Projects />;
-      case "skills":
-        return <Skills />;
-      case "achievements":
-        return <Achievements />;
-      default:
-        return <Error404 />;
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentTab: "home",
+        };
+        this.componentList = ["home", "about", "education", "experience", "projects", "skills", "achievements"];
     }
-  }
 
-  render() {
-    return (
-      <div>
-        <NavigationTabBar tabs={this.componentList} currentTab={this.state.currentTab} setTab={this.setTab} />
-        <Suspense fallback={Loading()}>{this.renderSwitch(this.state.currentTab)}</Suspense>
-        <Footer style={{ zIndex: -2 }} />
-      </div>
-    );
-  }
+    setTab = (newTab) => {
+        this.setState({ currentTab: newTab });
+    };
+
+    renderSwitch(currentTab) {
+        switch (currentTab) {
+            case "home":
+                return <Home />;
+            case "about":
+                return <About />;
+            case "education":
+                return <Education />;
+            case "experience":
+                return <Experience />;
+            case "projects":
+                return <Projects />;
+            case "skills":
+                return <Skills />;
+            case "achievements":
+                return <Achievements />;
+            default:
+                return <Error404 />;
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <NavigationTabBar tabs={this.componentList} currentTab={this.state.currentTab} setTab={this.setTab} />
+                <Suspense fallback={Loading()}>{this.renderSwitch(this.state.currentTab)}</Suspense>
+                <Footer style={{ zIndex: -2 }} />
+            </div>
+        );
+    }
 }
 
 export default App;
