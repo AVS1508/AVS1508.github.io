c52f179c66d885242f4a8fbd76cefe1536910b95
AVS1508 <aditya.11.present@gmail.com>
2021-07-29 09:57:59 +0530

    Formatting resolution for Error404.js

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Container, Row } from "react-bootstrap";
 
 const Error404 = () => {
-  return (
-    <Container className="center outer-structure">
-      <Row>
-        <h1>Sorry! This page does not exist!</h1>
-      </Row>
-    </Container>
-  );
+    return (
+        <Container className="center outer-structure">
+            <Row>
+                <h1>Sorry! This page does not exist!</h1>
+            </Row>
+        </Container>
+    );
 };
 
 export default Error404;
