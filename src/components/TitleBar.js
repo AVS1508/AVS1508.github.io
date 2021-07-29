e8dfa1f543c1bff18f7c4987ee0c1838668eb363
AVS1508 <aditya.11.present@gmail.com>
2021-07-29 09:56:00 +0530

    Formatting resolution for TitleBar.js

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,23 +1,23 @@
 import React from "react";
 
 const TitleBar = (props) => {
-  return (
-    <div className="title-bar justify-content-center" style={{ alignItems: "center", textAlign: "center" }}>
-      <div
-        style={{
-          alignItems: "center",
-          textAlign: "center",
-          margin: 0,
-          padding: "1em 2em",
-          borderRadius: "25px",
-          background: "var(--light-gradient)",
-          display: "inline-block",
-        }}
-      >
-        <h1>{props.title}</h1>
-      </div>
-    </div>
-  );
+    return (
+        <div className="title-bar justify-content-center" style={{ alignItems: "center", textAlign: "center" }}>
+            <div
+                style={{
+                    alignItems: "center",
+                    textAlign: "center",
+                    margin: 0,
+                    padding: "1em 2em",
+                    borderRadius: "25px",
+                    background: "var(--light-gradient)",
+                    display: "inline-block",
+                }}
+            >
+                <h1>{props.title}</h1>
+            </div>
+        </div>
+    );
 };
 
 export default TitleBar;
