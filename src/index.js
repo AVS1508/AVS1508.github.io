de40dc68ba1bb43b219cff4bcc704f9cb914c602
AVS1508 <aditya.11.present@gmail.com>
2021-07-29 09:56:46 +0530

    Formatting resolution for index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
