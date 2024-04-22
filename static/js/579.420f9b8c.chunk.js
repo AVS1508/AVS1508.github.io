d98ba94f3c6a84c1a45b13ade1c9fe8133dc9cde
Aditya Vikram Singh <aditya.11.present@gmail.com>
2024-04-22 15:17:00 -0400

    Updates

---
+++ static/js/579.420f9b8c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_website=self.webpackChunkpersonal_website||[]).push([[579],{4579:(e,s,o)=>{o.r(s),o.d(s,{default:()=>r});o(5043);const a="NotFound_container__1fusd",n="NotFound_title__D8KzW",t="NotFound_message__obVKA";var l=o(579);const r=()=>(0,l.jsxs)("div",{className:a,children:[(0,l.jsx)("h1",{className:n,children:"404 - Page Not Found"}),(0,l.jsx)("p",{className:t,children:"Sorry, the page you are looking for does not exist."})]})}}]);
+//# sourceMappingURL=579.420f9b8c.chunk.js.map
