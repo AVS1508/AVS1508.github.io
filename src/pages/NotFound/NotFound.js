8b2bf033ef5f7e681443872675e737d4a7dacdd8
Aditya Vikram Singh <aditya.11.present@gmail.com>
2024-04-21 20:16:09 -0400

    Styled the 404 page to be more descriptive

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "./NotFound.module.css";
 
 const NotFound = () => {
-  return <div>Hello, World! NotFound.</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>404 - Page Not Found</h1>
+      <p className={styles.message}>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
 };
 
 export default NotFound;
